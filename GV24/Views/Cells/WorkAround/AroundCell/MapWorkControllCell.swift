679b104118aded121ecaf63d96cc224e92a6805e
dinhphong <dinhphong10@gmail.com>
2017-08-31 17:31:35 +0700

    + import Kingfisher

--- GV24/Views/Cells/WorkAround/AroundCell/MapWorkControllCell.swift
+++ GV24/Views/Cells/WorkAround/AroundCell/MapWorkControllCell.swift
@@ -12,8 +12,12 @@ import GoogleMaps
 
 class MapWorkControllCell: UICollectionViewCell, CLLocationManagerDelegate, GMSMapViewDelegate {
     
-    
-    
+    var nearByWork: NearbyWork? {
+        didSet{
+            reloadMap()
+        }
+    }
+
     lazy var mapView : GMSMapView = {
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 16.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
@@ -41,5 +45,40 @@ class MapWorkControllCell: UICollectionViewCell, CLLocationManagerDelegate, GMSM
         mapView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0).isActive = true
         mapView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 0).isActive = true
         mapView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: 0).isActive = true
+    }
+    
+    //MARK: - mapview delegate
+    
+    func addMarkerFor(work : Work, at index : String ){
+        let marker : GMSMarker = GMSMarker(position: (work.info?.address?.location)!)
+        marker.title = index
+        marker.map = self.mapView
+    }
+    
+    func mapView(_ mapView: GMSMapView, markerInfoWindow marker: GMSMarker) -> UIView? {
+        let index = Int(marker.title!)
+        let window = MarkerInfoView(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+        window.work = nearByWork?.works?[index!]
+        return window
+    }
+    func mapView(_ mapView: GMSMapView, didTapInfoWindowOf marker: GMSMarker) {
+        /*if UserHelpers.isLogin{
+            if let index = Int(marker.title!){
+                let maidProfileVC = MaidProfileVC()
+                maidProfileVC.maid = maids?[index]
+                self.present(viewController: maidProfileVC)
+            }
+        }else{
+            showAlertLogin()
+        }*/
+        
+    }
+    func reloadMap(){
+        self.mapView.clear()
+        var index = 0
+        for work in (nearByWork?.works)! {
+            self.addMarkerFor(work: work, at: "\(index)")
+            index += 1
+        }
     }
 }
